6b11be2da35044ef78cde1602dc0b6e809f87d62
akanandhu <87613645+akanandhu@users.noreply.github.com>
2022-08-30 14:38:54 +0530

    Add files via upload

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './NavBar.css';
+
+function NavBar() {
+  return (
+    <div className='navbar'>
+            <img className='logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="Netlix logo" />
+            <img className='avatar' src="https://media.istockphoto.com/vectors/avatar-flat-icon-on-black-background-black-style-vector-illustration-vector-id1161086164?k=20&m=1161086164&s=170667a&w=0&h=x_w1-m_iLtVMAtae9ysSGLDglPFhq-qt3OCu5Ur3boQ=" alt="Avatar" />
+    </div>
+  )
+}
+export default NavBar
